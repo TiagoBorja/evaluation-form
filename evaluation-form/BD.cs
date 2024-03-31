01e63fba350969789c0f1af29c31865f3e1ed985
Tiago de Borja Reis Rodrigues <158228629+TiagoBorja@users.noreply.github.com>
2024-03-31 22:57:19 +0100

    Classe para a BD

---
+++ evaluation-form/BD.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+namespace evaluation_form
+{
+    internal class BD
+    {
+        public static string data_source = "datasource=localhost;username=root;password=;database=evaluation-form";
+
+
+        public static MySqlConnection ConectarBD()
+        {
+            MySqlConnection conexao = new MySqlConnection(data_source);
+            try
+            {
+                conexao.Open();
+                return conexao;
+            }
+            catch
+            {
+                conexao.Dispose();
+                return null;
+            }
+        }
+    }
+}
